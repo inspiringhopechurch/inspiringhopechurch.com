c2f57200a0f0a6ecb512d376e1b208db7186dd3f
Orville Bennett <dev@thebennettproject.com>
2021-01-16 03:49:46 -0500

    chore: Remove superfluous use of string templates.

--- src/components/serviceItem.js
+++ src/components/serviceItem.js
@@ -5,20 +5,20 @@ import "./serviceItem.sass";
 
 /// ServiceItem shows icon conditionally
 const ServiceItem = ({ children, ...props }) => (
-  <div className={`column is-half-tablet is-one-third-desktop is-one-third-fullhd`}>
-    <article className={`media px-5`}>
+  <div className="column is-half-tablet is-one-third-desktop is-one-third-fullhd">
+    <article className="media">
       {props.icon && (
-        <figure className={`media-left`}>
-          <p className={`image is-48x48`}>
+        <figure className="media-left">
+          <p className="image is-48x48">
             <FontAwesomeIcon icon={props.icon} size={props.iconSize} />
           </p>
         </figure>
       )}
-      <div className={`media-content`}>
-        <h2 className={`title is-size-3 is-uppercase`}>
-          <span className={`has-text-weight-light`}>Our</span> <span className={`decorated`}>{props.serviceName}</span>
+      <div className="media-content">
+        <h2 className="title is-size-3 is-uppercase">
+          <span className="has-text-weight-light">Our</span> <span className="decorated">{props.serviceName}</span>
         </h2>
-        <p className={`content is-medium`}>{children}</p>
+        <p className="content is-medium">{children}</p>
       </div>
     </article>
   </div>
