9cf66be486f77182c3b8e9219f4566f2c130714d
Orville Bennett <dev@thebennettproject.com>
2021-03-10 23:43:38 -0500

    chore: Disable babel changes, add preset config.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Layout from "./src/templates/layout";
 // For IE 11
-import "babel-polyfill";
+// import "@babel/polyfill";
 import "./src/styles/global.sass";
 
 // export function onRouteUpdate({ location, prevLocation }) {
