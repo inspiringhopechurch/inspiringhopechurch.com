259c637bbf0b9ed3ce2a8a4f9c6d9979f0ffb4ac
Orville Bennett <dev@thebennettproject.com>
2021-06-08 23:27:37 -0400

    chore: Remove IE11 babel polyfill and end IE11 support.
    - No presence in analytics.

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,10 +1,6 @@
 import React from "react";
 import Layout from "./src/templates/layout";
-// For IE 11
-// if (!global._babelPolyfill) {
-//     require('@babel/polyfill')
-// // import "@babel/polyfill";
-// }
+
 import "./src/styles/global.sass";
 
 export function wrapPageElement({ element, props }) {
