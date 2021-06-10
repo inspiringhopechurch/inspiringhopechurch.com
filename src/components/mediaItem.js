8326946a069d891973aa589064a62a2bc8ed752e
Orville Bennett <dev@thebennettproject.com>
2021-06-10 16:22:27 -0400

    fix: Show live-streamed content properly.

--- src/components/mediaItem.js
+++ src/components/mediaItem.js
@@ -1,35 +1,36 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Link } from "gatsby";
-import { cleanHtml } from "../utils";
+import { cleanHtml, cleanHtmlForVideo } from "../utils";
 
 /**
  * Component that shows a block of media, such as audio or video, that can be played by the user.
  * @param {object} props
  * @param {string} props.title
- * @param {string=} props.category
  * @param {string} props.description
  * @param {string} props.link
+ * @param {string=} props.category
  * @param {string=} props.imgSrc
  * @param {string=} props.vidSrc
  */
 const MediaItem = ({
   title,
-  category = "Sunday Message",
   description,
   link,
+  category = "Sunday Message",
   imgSrc = "https://cdn.emk.dev/templates/featured-image.png",
   vidSrc
 }) => {
   const ifIsVideo = !!vidSrc; // should check if file ext is mp4, webm or av1 (or heic?)
   const ifIsAudio = false; // check if file ext is mp3
+  const isLive = vidSrc.includes('inspiringhopechurch.com');
 
   return (
     <div className="column is-12 post">
       <article className="columns featured">
         {ifIsVideo
           ? <div className="column is-7 post-img"
-            dangerouslySetInnerHTML={vidSrc && cleanHtml(vidSrc)} />
+            dangerouslySetInnerHTML={vidSrc && isLive ? cleanHtml(vidSrc) : cleanHtmlForVideo(vidSrc)} />
           : <div className="column is-7 post-img ">
             <img src={imgSrc} alt="" />
           </div>
@@ -40,7 +41,7 @@ const MediaItem = ({
             <h1 className="title post-title">{title}</h1>
             <p className="post-excerpt">{description}</p>
             <br />
-            {!vidSrc.includes('youtube.com') && !vidSrc.includes('inspiringhopechurch.com') && <Link to={link} className="button is-link">
+            {!vidSrc.includes('youtube.com') && !isLive && <Link to={link} className="button is-link">
               {ifIsVideo ? "Watch Video" : ifIsAudio ? "Listen Audio" : `Go to ${category}`}
             </Link>}
           </div>
@@ -56,6 +57,7 @@ MediaItem.propTypes = {
   description: PropTypes.string.isRequired,
   link: PropTypes.string,
   imgSrc: PropTypes.string,
+  vidSrc: PropTypes.string,
 };
 
 export default MediaItem;
