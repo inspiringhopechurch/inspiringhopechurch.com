cd64bce115c6cfe8f168cb8b99e21b440059292b
Orville Bennett <dev@thebennettproject.com>
2021-10-11 22:31:26 -0400

    test: Upgrade jest to v27.

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,7 @@ module.exports = {
     ".+\\.(css|styl|less|sass|scss)$": `identity-obj-proxy`,
     ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|vtt)$": `<rootDir>/__mocks__/file-mock.js`,
   },
+  testEnvironment: "jsdom",
   testPathIgnorePatterns: [`cypress`, `src/fixtures`, `node_modules`, `.cache`],
   transformIgnorePatterns: [`node_modules/(?!(gatsby)/)`],
   globals: {
