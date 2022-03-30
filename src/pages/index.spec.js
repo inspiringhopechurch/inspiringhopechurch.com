e60eb294b88d46fcffb85f9b2cb255b023eb8cce
Orville Bennett <dev@thebennettproject.com>
2022-03-30 09:07:05 -0400

    tests: Mock mapbox import to avoid jest failure.

--- src/pages/index.spec.js
+++ src/pages/index.spec.js
@@ -2,6 +2,7 @@ import React from "react";
 import { shallow } from "enzyme";
 import Index from "./index";
 import data from "../fixtures/data";
+jest.mock("../components/map", () => () => <></>);
 
 describe("Index", () => {
   it("should render without crashing", () => {
