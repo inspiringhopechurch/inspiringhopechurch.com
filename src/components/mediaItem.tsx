9a98353fd8ed8e939d8441db9a65590012be709d
Orville Bennett <dev@thebennettproject.com>
2022-12-15 13:34:15 -0500

    feat: Convert MediaItem and VideoPlayer components to typescript.

---
+++ src/components/mediaItem.tsx
@@ -0,0 +1,78 @@
+import React, { useEffect, useRef, useState } from "react";
+// import { Link } from "gatsby";
+import VideoPlayer from "./videoPlayer";
+import { cleanHtml, cleanHtmlForVideo } from "../utils";
+
+/**
+ * Component that shows a block of media, such as audio or video, that can be played by the user.
+ */
+const MediaItem = ({
+  title,
+  description,
+  // link,
+  category = "Sunday Message",
+  imgSrc = "/assets/ghost-featured-image.png",
+  vidSrc,
+  timestamp
+}: MediaItemProps) => {
+  const [renderVideo, setRenderVideo] = useState({ nativeMarkup: true, videoId: '' })
+  const videoEl = useRef<HTMLElement | null>(null)
+  const ifIsVideo = !!vidSrc; // should check if file ext is mp4, webm or av1 (or heic?)
+  // const ifIsAudio = false; // check if file ext is mp3
+  const isLive = vidSrc ? vidSrc.includes('inspiringhopechurch.com') : false;
+
+  useEffect(() => {
+    const videoId = videoEl?.current?.children[0].children[0].id
+    videoId && videoId !== '' &&
+      setRenderVideo({
+        nativeMarkup: false,
+        videoId
+      })
+  }, [])
+
+  return (
+    <div className="column is-12 post">
+      <article ref={videoEl} className="columns featured">
+        {ifIsVideo ?
+          (renderVideo.nativeMarkup ?
+            <div className="column is-7 post-img" dangerouslySetInnerHTML={vidSrc && isLive ? cleanHtml(vidSrc) : cleanHtmlForVideo(vidSrc)} /> :
+            <div className="column is-7 post-img">
+              <VideoPlayer
+                caption={{ en: `/assets/${renderVideo.videoId}.en.vtt` }}
+                src={{ mp4: `//ihc-video-storage.us-east-1.linodeobjects.com/assets/${renderVideo.videoId}.mp4` }}
+                // posterImg={`/assets/${renderVideo.videoId}.jpg`}
+                id={renderVideo.videoId}
+                preload
+              />
+            </div>) :
+          <div className="column is-7 post-img">
+            <img src={imgSrc} alt="" />
+          </div>
+        }
+        <div className="column is-5 featured-content is-vcentered">
+          <div>
+            <h2 className="heading post-category">{!timestamp ? category : `${category} | ${timestamp.split('T')[0]}`}</h2>
+            <h1 className="title post-title">{title}</h1>
+            <p className="post-excerpt">{description}</p>
+            <br />
+            {/* {!vidSrc?.includes('youtube.com') && !isLive && <Link to={link} className="button is-link">
+              {ifIsVideo ? "Watch Video" : ifIsAudio ? "Listen Audio" : `Go to ${category}`}
+            </Link>} */}
+          </div>
+        </div>
+      </article>
+    </div>
+  );
+};
+
+type MediaItemProps = {
+  title: string,
+  category?: string,
+  description: string,
+  link?: string,
+  imgSrc?: string,
+  vidSrc?: string,
+  timestamp?: string,
+};
+
+export default MediaItem;
---
+++ src/components/videoPlayer.tsx
@@ -0,0 +1,79 @@
+import React, { useEffect, useRef } from "react";
+
+/**
+ * Video Player component
+ */
+const VideoPlayer = ({ id, caption, src, preload, posterImg }: VPProps) => {
+  const vidRef = useRef(null)
+
+  useEffect(() => {
+    let player;
+    import("plyr").then(component => {
+      player = new component.default(`[id="${id}-video"]`, {
+        resetOnEnd: true,
+        disableContextMenu: false,
+        enabled: true,
+        fullscreen: { enabled: true, fallback: true, }
+      });
+    })
+
+    return () => { player = null }; // hopefully cleanup when gc happens;
+  }, [])
+
+  return (
+    <video
+      ref={vidRef}
+      className="has-ratio"
+      controls
+      id={`${id}-video`}
+      width="100%"
+      height="100%"
+      poster={posterImg ? posterImg : `/assets/video-logo.svg`}
+      preload={preload ? "metadata" : "none"}
+      playsInline
+    >
+      {src.webm && <source src={src.webm} type="video/webm" />}
+      <source src={src.mp4} type="video/mp4"
+      />
+      {caption &&
+        <track
+          kind="subtitles"
+          label="English"
+          srcLang="en"
+          src={caption.en}
+        />}
+      {caption?.es &&
+        <track
+          kind="subtitles"
+          label="Español"
+          srcLang="es"
+          src={caption.es}
+        />}
+      Unfortunately your browser is old and does not support embedded videos. Please consider upgrading.
+    </video>
+  )
+};
+
+type VPProps = {
+  /** File identifier */
+  id: string,
+  /** Captions object */
+  caption?: {
+    /** English language captions path */
+    en: string,
+    /** Spanish language captions path */
+    es?: string
+  },
+  src: {
+    /** mp4 file source path */
+    mp4: string,
+    /** webm file source path */
+    webm?: string,
+  }
+  /** Poster image source path */
+  posterImg?: string,
+  /** Whether to preload metadata or not */
+  preload?: boolean,
+};
+
+export default VideoPlayer;
